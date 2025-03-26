60bbd6d189e81e0a3c091b6dcb2332a554c4b52d
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-03-26 10:53:39 +0530

    added riverpod state management

--- lib/country_page.dart
+++ lib/country_page.dart
@@ -1,103 +1,23 @@
-import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:countries_api_demo/country_model.dart';
-import 'package:countries_api_demo/db_helper.dart';
 import 'package:countries_api_demo/flag_page.dart';
+import 'package:countries_api_demo/provider/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CountryPage extends StatefulWidget {
+class CountryPage extends ConsumerWidget {
   const CountryPage({super.key});
 
   @override
-  State<CountryPage> createState() => _CountryPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final countryState = ref.watch(countryProvider);
 
-class _CountryPageState extends State<CountryPage> {
-  List<CountryModel> allCountries = [];
-  List<CountryModel> displayedCountries = [];
-
-  String searchQuery = '';
-
-  DBHelper? dbRef;
-
-  bool isAscending = true;
-
-  @override
-  void initState() {
-    dbRef = DBHelper.getInstance;
-    getCountriesData();
-    super.initState();
-  }
-
-  Future<List<CountryModel>> fetchCountriesFromAPI() async {
-    final response = await http.get(Uri.parse(
-        'https://restcountries.com/v3.1/all?fields=name,flags,cca2,currencies'));
-
-    if (response.statusCode == 200) {
-      List<dynamic> jsonList = jsonDecode(response.body);
-      return jsonList.map((json) => CountryModel.fromJson(json)).toList();
-    } else {
-      return throw Exception('Failed to load data');
-    }
-  }
-
-  Future<void> getCountriesData() async {
-    final List<Map<String, dynamic>> countries = await dbRef!.getAllCountries();
-
-    List<CountryModel> displayAllCountries =
-        countries.map((country) => CountryModel.fromDatabase(country)).toList();
-
-    if (displayAllCountries.isEmpty) {
-      List<CountryModel> displayAllCountries = await fetchCountriesFromAPI();
-
-      List<Map<String, dynamic>> dbMaps =
-          displayAllCountries.map((country) => country.toDatabase()).toList();
-
-      await dbRef!.insertCountries(dbMaps);
-    }
-
-    setState(() {
-      allCountries = displayAllCountries;
-    });
-    applyFilter();
-  }
-
-  void applyFilter() {
-    List<CountryModel> filteredList = allCountries
-        .where((country) =>
-            (country.name!.common)!.toLowerCase().contains(searchQuery))
-        .toList();
-
-    filteredList.sort((a, b) => isAscending
-        ? (a.cca2!).compareTo(b.cca2!)
-        : (b.cca2!).compareTo(a.cca2!));
-
-    setState(() {
-      displayedCountries = filteredList;
-    });
-  }
-
-  void deleteCountry(CountryModel country) async {
-    await dbRef!.deleteCountry(country.cca2!);
-    setState(() {
-      allCountries.removeWhere((c) => c.cca2 == country.cca2);
-    });
-    applyFilter();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Countries'),
           actions: [
             IconButton(
                 onPressed: () {
-                  setState(() {
-                    isAscending = !isAscending;
-                    applyFilter();
-                  });
+                  ref.read(countryProvider.notifier).toggleSort();
                 },
                 icon: Icon(Icons.sort))
           ],
@@ -108,10 +28,7 @@ class _CountryPageState extends State<CountryPage> {
               padding: EdgeInsets.all(20.0),
               child: TextField(
                 onChanged: (String text) {
-                  searchQuery = text;
-                  setState(() {
-                    applyFilter();
-                  });
+                  ref.read(countryProvider.notifier).searchQuery(text);
                 },
                 decoration: InputDecoration(
                     prefixIcon: Icon(Icons.search),
@@ -120,49 +37,56 @@ class _CountryPageState extends State<CountryPage> {
                         borderRadius: BorderRadius.circular(30))),
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: displayedCountries.length,
-                  itemBuilder: (context, index) {
-                    var country = displayedCountries[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Card(
-                        child: ListTile(
-                          leading: GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          FlagPage(country: country)));
-                            },
-                            child: Hero(
-                              tag: country.cca2!,
-                              child: SizedBox(
-                                height: 40,
-                                width: 50,
-                                child: CachedNetworkImage(
-                                  imageUrl: '${country.flags!.png}',
-                                  placeholder: (context, url) =>
-                                      CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) =>
-                                      Icon(Icons.error),
+            if (countryState.isLoading == true)
+              Center(
+                child: CircularProgressIndicator(),
+              )
+            else
+              Expanded(
+                child: ListView.builder(
+                    itemCount: countryState.displayedCountries.length,
+                    itemBuilder: (context, index) {
+                      var country = countryState.displayedCountries[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Card(
+                          child: ListTile(
+                            leading: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            FlagPage(country: country)));
+                              },
+                              child: Hero(
+                                tag: country.cca2!,
+                                child: SizedBox(
+                                  height: 40,
+                                  width: 50,
+                                  child: CachedNetworkImage(
+                                    imageUrl: '${country.flags!.png}',
+                                    placeholder: (context, url) =>
+                                        CircularProgressIndicator(),
+                                    errorWidget: (context, url, error) =>
+                                        Icon(Icons.error),
+                                  ),
                                 ),
                               ),
                             ),
+                            title: Text('${country.name!.common}'),
+                            subtitle: Text(
+                                'code: ${country.cca2}  currency: ${country.currencies!.values.first.symbol}'),
+                            onLongPress: () {
+                              ref
+                                  .read(countryProvider.notifier)
+                                  .deleteCountry(country);
+                            },
                           ),
-                          title: Text('${country.name!.common}'),
-                          subtitle: Text(
-                              'code: ${country.cca2}  currency: ${country.currencies!.values.first.symbol}'),
-                          onLongPress: () {
-                            deleteCountry(country);
-                          },
                         ),
-                      ),
-                    );
-                  }),
-            ),
+                      );
+                    }),
+              ),
           ],
         ));
   }
---
+++ lib/provider/providers.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+
+import 'package:countries_api_demo/country_model.dart';
+import 'package:countries_api_demo/db_helper.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+class CountryState {
+  final List<CountryModel> allCountries;
+  final List<CountryModel> displayedCountries;
+  final String searchQuery;
+  final bool isAscending;
+  final bool isLoading;
+
+  CountryState({
+    this.allCountries = const [],
+    this.displayedCountries = const [],
+    this.searchQuery = '',
+    this.isAscending = true,
+    this.isLoading = false,
+  });
+
+  CountryState copyWith({
+    List<CountryModel>? allCountries,
+    List<CountryModel>? displayedCountries,
+    String? searchQuery,
+    bool? isAscending,
+    bool? isLoading,
+  }) {
+    return CountryState(
+      allCountries: allCountries ?? this.allCountries,
+      displayedCountries: displayedCountries ?? this.displayedCountries,
+      searchQuery: searchQuery ?? this.searchQuery,
+      isAscending: isAscending ?? this.isAscending,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
+}
+
+class CountryNotifier extends StateNotifier<CountryState> {
+  final DBHelper? dbRef;
+
+  CountryNotifier(this.dbRef) : super(CountryState()) {
+    getCountriesData();
+  }
+
+  Future<List<CountryModel>> fetchCountriesFromAPI() async {
+    final response = await http.get(Uri.parse(
+        'https://restcountries.com/v3.1/all?fields=name,flags,cca2,currencies'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = jsonDecode(response.body);
+      return jsonList.map((json) => CountryModel.fromJson(json)).toList();
+    } else {
+      return throw Exception('Failed to load data');
+    }
+  }
+
+  Future<void> getCountriesData() async {
+    final List<Map<String, dynamic>> countries = await dbRef!.getAllCountries();
+
+    List<CountryModel> displayAllCountries =
+        countries.map((country) => CountryModel.fromDatabase(country)).toList();
+
+    if (displayAllCountries.isEmpty) {
+      List<CountryModel> displayAllCountries = await fetchCountriesFromAPI();
+
+      List<Map<String, dynamic>> dbMaps =
+          displayAllCountries.map((country) => country.toDatabase()).toList();
+
+      await dbRef!.insertCountries(dbMaps);
+    }
+
+    state = state.copyWith(allCountries: displayAllCountries, isLoading: false);
+  }
+
+  void applyFilter() {
+    List<CountryModel> filteredList = state.allCountries
+        .where((country) =>
+            (country.name!.common)!.toLowerCase().contains(state.searchQuery))
+        .toList();
+
+    filteredList.sort((a, b) => state.isAscending
+        ? (a.cca2!).compareTo(b.cca2!)
+        : (b.cca2!).compareTo(a.cca2!));
+
+    state = state.copyWith(displayedCountries: filteredList);
+  }
+
+  void searchQuery(String query) {
+    state = state.copyWith(searchQuery: query.toLowerCase());
+    applyFilter();
+  }
+
+  void toggleSort() {
+    state = state.copyWith(isAscending: !state.isAscending);
+    applyFilter();
+  }
+
+  void deleteCountry(CountryModel country) async {
+    await dbRef!.deleteCountry(country.cca2!);
+    final newAllCountries = [...state.allCountries]
+      ..removeWhere((c) => c.cca2 == country.cca2);
+
+    state = state.copyWith(allCountries: newAllCountries);
+    applyFilter();
+  }
+}
+
+final dbProvider = Provider((ref) => DBHelper.getInstance);
+
+final countryProvider =
+    StateNotifierProvider<CountryNotifier, CountryState>((ref) {
+  final dbRef = ref.watch(dbProvider);
+  return CountryNotifier(dbRef);
+});
